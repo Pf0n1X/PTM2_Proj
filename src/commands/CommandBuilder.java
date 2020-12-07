5f72240ddab36fa5da4abb925f88f40490831286
Pf0n1X <bboypsyfly@gmail.com>
2020-12-07 19:35:08 +0200

    CommandBuilder and interpreter changes

--- src/commands/CommandBuilder.java
+++ src/commands/CommandBuilder.java
@@ -1,7 +1,47 @@
 package commands;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public abstract class CommandBuilder {
-	public abstract Command build(ArrayList<String> args);
+import interpreter.Interpreter;
+
+public class CommandBuilder {
+	
+	// Data Members
+	private HashMap<String, Command> commandMap;
+	
+	// Cosntructors
+	public CommandBuilder(Interpreter interpreter) {
+		this.setCommandMap(new HashMap<String, Command>());
+//		this.getCommandMap().put("+", new Plus(null, null));
+//		this.getCommandMap().put("-", new Minus(null, null));
+//		this.getCommandMap().put("*", new Mul(null, null));
+//		this.getCommandMap().put("/", new Div(null, null));
+		this.getCommandMap().put("return", new ReturnCommand(interpreter));
+		this.getCommandMap().put("var", new DefineVarCommand(interpreter));
+		this.getCommandMap().put("openDataServer", new OpenServerCommand(interpreter)); // TODO: Add port and rate settings
+		this.getCommandMap().put("while", new WhileCommand(interpreter));
+		this.getCommandMap().put("if", new IfCommand(interpreter));
+		this.getCommandMap().put("sleep", new SleepCommand(interpreter));
+	}
+	
+	// Methods
+	public Command getCommand (String commandName) {
+		return this.getCommandMap().get(commandName);
+	};
+	
+	// Getters & Setters
+	/**
+	 * @return the commandMap
+	 */
+	public HashMap<String, Command> getCommandMap() {
+		return commandMap;
+	}
+
+	/**
+	 * @param commandMap the commandMap to set
+	 */
+	public void setCommandMap(HashMap<String, Command> commandMap) {
+		this.commandMap = commandMap;
+	}
 }
--- src/interpreter/Interpreter.java
+++ src/interpreter/Interpreter.java
@@ -8,6 +8,7 @@ import java.util.regex.Pattern;
 import javax.sound.sampled.Line;
 
 import commands.Command;
+import commands.CommandBuilder;
 import commands.DefineVarCommand;
 import commands.IfCommand;
 import commands.OpenServerCommand;
@@ -23,7 +24,11 @@ public class Interpreter {
 	// Data Members
 	private HashMap<String, Command> commandMap;
 	private SymbolTable symbolTable;
-	
+	private int tokenIndex;
+	private int tokenBlockIndex;
+	private double returnedValue;
+	private ArrayList<String[]> tokens;
+	private CommandBuilder commandBuilder;
 	
 	// Constant Members
 	private static final String lexerMatch = "(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}|-?\\d+\\.?\\d*|-?\\d*\\.?\\d+|\".*\"|==|!=|<=|>=|<|>|\\+|-|\\*|\\/|&&|\\|\\||!|=|\\(|\\)|\\{|\\}|\\w+)";
@@ -32,21 +37,80 @@ public class Interpreter {
 	public Interpreter() {
 		
 		// Initialize the command map.
-		this.setCommandMap(new HashMap<String, Command>());
-//		this.getCommandMap().put("+", new Plus(null, null));
-//		this.getCommandMap().put("-", new Minus(null, null));
-//		this.getCommandMap().put("*", new Mul(null, null));
-//		this.getCommandMap().put("/", new Div(null, null));
-		this.getCommandMap().put("return", new ReturnCommand());
-		this.getCommandMap().put("var", new DefineVarCommand());
-		this.getCommandMap().put("openDataServer", new OpenServerCommand()); // TODO: Add port and rate settings
-		this.getCommandMap().put("while", new WhileCommand(null));
-		this.getCommandMap().put("if", new IfCommand(null));
-		this.getCommandMap().put("sleep", new SleepCommand(null));
-		
+		this.setCommandBuilder(new CommandBuilder(this));
 	}
 
 	// Getters & Setters
+	/**
+	 * @return the commandBuilder
+	 */
+	public CommandBuilder getCommandBuilder() {
+		return commandBuilder;
+	}
+
+	/**
+	 * @param commandBuilder the commandBuilder to set
+	 */
+	public void setCommandBuilder(CommandBuilder commandBuilder) {
+		this.commandBuilder = commandBuilder;
+	}
+	
+	/**
+	 * @return the tokenIndex
+	 */
+	public int getTokenIndex() {
+		return tokenIndex;
+	}
+
+	/**
+	 * @param tokenIndex the tokenIndex to set
+	 */
+	public void setTokenIndex(int tokenIndex) {
+		this.tokenIndex = tokenIndex;
+	}
+
+	/**
+	 * @return the tokenBlockIndex
+	 */
+	public int getTokenBlockIndex() {
+		return tokenBlockIndex;
+	}
+
+	/**
+	 * @param tokenBlockIndex the tokenBlockIndex to set
+	 */
+	public void setTokenBlockIndex(int tokenBlockIndex) {
+		this.tokenBlockIndex = tokenBlockIndex;
+	}
+
+	/**
+	 * @return the returnedValue
+	 */
+	public double getReturnedValue() {
+		return returnedValue;
+	}
+
+	/**
+	 * @param returnedValue the returnedValue to set
+	 */
+	public void setReturnedValue(double returnedValue) {
+		this.returnedValue = returnedValue;
+	}
+
+	/**
+	 * @return the tokens
+	 */
+	public ArrayList<String[]> getTokens() {
+		return tokens;
+	}
+
+	/**
+	 * @param tokens the tokens to set
+	 */
+	public void setTokens(ArrayList<String[]> tokens) {
+		this.tokens = tokens;
+	}
+
 	/**
 	 * @return the commandMap
 	 */
@@ -102,10 +166,10 @@ public class Interpreter {
 	public void parse(ArrayList<String[]> tokens) {
 		
 		// Go through every token block(AKA line).
-		for (int tokenBlockIndex = 0; tokenBlockIndex < tokens.size(); tokenBlockIndex++) {
+		for (tokenBlockIndex = 0; tokenBlockIndex < tokens.size(); tokenBlockIndex++) {
 			
 			// Go through the key words.
-			for (int tokenIndex = 0; tokenIndex < tokens.get(tokenBlockIndex).length; tokenIndex++) {
+			for (tokenIndex = 0; tokenIndex < tokens.get(tokenBlockIndex).length; tokenIndex++) {
 				System.out.println("T1");
 				System.out.println(tokens.get(tokenBlockIndex)[tokenIndex]);
 				Command command = this.getCommandMap().get(tokens.get(tokenBlockIndex)[tokenIndex]);
